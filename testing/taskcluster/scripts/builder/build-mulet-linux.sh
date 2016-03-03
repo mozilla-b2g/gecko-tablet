79c4abdda27ab7aa8278313e53e1fd173c52198d
Ghislain 'Aus' Lacroix <aus@mozilla.com>
2016-03-03 10:33:44 -0800

    Bug 1253002 - Update JSON manifest file after moving assets. Go back to 'target' name.

--- testing/taskcluster/scripts/builder/build-mulet-linux.sh
+++ testing/taskcluster/scripts/builder/build-mulet-linux.sh
@@ -28,15 +28,18 @@ mkdir -p $HOME/artifacts/
 
 # Discard version numbers from packaged files, they just make it hard to write
 # the right filename in the task payload where artifacts are declared
-mv *.linux-x86_64.tar.bz2       $HOME/artifacts/firefox.linux-x86_64.tar.bz2
-mv *.linux-x86_64.json          $HOME/artifacts/firefox.linux-x86_64.json
+mv *.linux-x86_64.tar.bz2       $HOME/artifacts/target.linux-x86_64.tar.bz2
+mv *.linux-x86_64.json          $HOME/artifacts/target.linux-x86_64.json
 for name in common cppunittest reftest mochitest xpcshell web-platform; do
-    mv *.$name.tests.zip          $HOME/artifacts/firefox.$name.tests.zip ;
+    mv *.$name.tests.zip          $HOME/artifacts/target.$name.tests.zip ;
 done
-mv *.test_packages.json         $HOME/artifacts/firefox.test_packages.json
-mv *.crashreporter-symbols.zip  $HOME/artifacts/firefox.crashreporter-symbols.zip
+mv *.test_packages.json         $HOME/artifacts/target.test_packages.json
+mv *.crashreporter-symbols.zip  $HOME/artifacts/target.crashreporter-symbols.zip
 mv jsshell-linux-x86_64.zip     $HOME/artifacts/jsshell-linux-x86_64.zip
 mv mozharness.zip               $HOME/artifacts/mozharness.zip
+
+# Fix up the JSON manifest file since we've _renamed_ assets.
+sed -i 's/firefox.*en\-US/target/g' $HOME/artifacts/target.test_packages.json
 
 ccache -s
 
