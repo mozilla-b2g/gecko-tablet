9024ce917b39b76b1614d2f37e2615829c8863f9
Ghislain 'Aus' Lacroix <aus@mozilla.com>
2016-03-10 14:37:46 -0800

    Sort out copying of dist/* assets to artifacts.

--- testing/taskcluster/scripts/builder/build-mulet-linux.sh
+++ testing/taskcluster/scripts/builder/build-mulet-linux.sh
@@ -27,14 +27,7 @@ mkdir -p $HOME/artifacts/
 
 # Discard version numbers from packaged files, they just make it hard to write
 # the right filename in the task payload where artifacts are declared
-mv *.linux-x86_64.tar.bz2       $HOME/artifacts/target.linux-x86_64.tar.bz2
-mv *.linux-x86_64.json          $HOME/artifacts/target.linux-x86_64.json
-for name in common cppunittest reftest mochitest xpcshell web-platform; do
-    mv *.$name.tests.zip          $HOME/artifacts/target.$name.tests.zip ;
-done
-mv *.test_packages.json         $HOME/artifacts/target.test_packages.json
-mv *.crashreporter-symbols.zip  $HOME/artifacts/target.crashreporter-symbols.zip
-mv jsshell-linux-x86_64.zip     $HOME/artifacts/jsshell-linux-x86_64.zip
+mv target.*                     $HOME/artifacts
 mv mozharness.zip               $HOME/artifacts/mozharness.zip
 
 ccache -s
