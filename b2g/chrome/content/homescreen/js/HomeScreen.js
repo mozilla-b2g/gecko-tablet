888abe8fce36e985d03d0c701eb9cece86431079
Ben Francis <ben@tola.me.uk>
2016-03-24 20:09:46 +0000

    Dynamically update home screen using BroadcastChannel

--- b2g/chrome/content/homescreen/js/HomeScreen.js
+++ b2g/chrome/content/homescreen/js/HomeScreen.js
@@ -5,6 +5,10 @@
  */
 
 var HomeScreen = {
+  /**
+   * Broadcast channel used to communicate with the system.
+   */
+  broadcastChannel: null,
 
   /**
    * Start the home screen.
@@ -17,18 +21,29 @@ var HomeScreen = {
     }).bind(this), function(error) {
       console.error('Failed to start Places database ' + error);
     });
+    this.broadcastChannel = new BroadcastChannel('system');
+    this.broadcastChannel.onmessage = this.handleMessage.bind(this);
   },
 
   /*
    * Show top sites.
    */
   showTopSites: function() {
+    this.topSites.innerHTML = '';
     Places.getTopSites().then((function(topSites) {
       topSites.forEach(function(siteObject) {
         var tile = new Tile(siteObject);
       }, this);
     }).bind(this));
-  }
+  },
+
+  /**
+   * Handle a message received via postMessage().
+   */
+  handleMessage: function(event) {
+    this.showTopSites();
+    console.log('Received message saying ' + event.data);
+  },
 
 };
 
