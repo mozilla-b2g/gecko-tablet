107cade6e9be52c308b7e29621f07fc68f6d1935
Ben Francis <ben@tola.me.uk>
2016-04-29 17:32:38 +0100

    Populate default sites

--- b2g/chrome/content/homescreen/js/Tile.js
+++ b2g/chrome/content/homescreen/js/Tile.js
@@ -16,12 +16,21 @@ var Tile = function(siteObject) {
 
 
 /** 
- * Tile View.
+ * Generate Tile View.
  */
 Tile.prototype.view = function() {
-  return '<li id="tile-' + this.siteObject.hostname +'" class="tile"><span class="tile-name">' +
-    this.siteObject.hostname
-    + '</span></li>';
+  var style = '';
+  if (this.siteObject.backgroundColor) {
+    style += 'background-color: ' + this.siteObject.backgroundColor + ';';
+    var rgb = this._hexToRgb(this.siteObject.backgroundColor);
+    var textColor = this._blackOrWhite(rgb);
+    style +='color: ' + textColor + ';';
+  }
+  if (this.siteObject.icons && this.siteObject.icons[0])
+    style += 'background-image: url(' + this.siteObject.icons[0].src +  ');'
+  var label = this.siteObject.name || this.siteObject.id;
+  return '<li id="tile-' + this.siteObject.id +'" class="tile" style="'
+    + style + '"><span class="tile-name">' + label + '</span></li>';
 };
 
 /**
@@ -29,10 +38,43 @@ Tile.prototype.view = function() {
  */
 Tile.prototype.render = function() {
   this.container.insertAdjacentHTML('beforeend', this.view());
-  this.element = document.getElementById('tile-' + this.siteObject.hostname);
+  this.element = document.getElementById('tile-' + this.siteObject.id);
   this.element.addEventListener('click', this.open.bind(this));
 };
 
+/**
+ * Launch site in a new window.
+ */
 Tile.prototype.open = function() {
   window.open(this.siteObject.startUrl);
+  console.log('opening window at ' + this.siteObject.startUrl);
+};
+
+/**
+ * Convert hex color value to rgb.
+ *
+ * @argument {String} hex color string e.g. #ff0000
+ * @returns {Object} RGB object with separate r, g and b properties
+ */
+Tile.prototype._hexToRgb = function(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  } : null;
+};
+
+/**
+ * Choose highest contrast text colour.
+ *
+ * @param {Object} RGB object with r, g, b properties.
+ * @return {String} black or white hex colour code.
+ */
+Tile.prototype._blackOrWhite = function(rgb) {
+  if ((rgb.r*0.299 + rgb.g*0.587 + rgb.b*0.114) > 186) {
+    return '#000'; 
+  } else {
+    return '#fff';
+  }
 };
