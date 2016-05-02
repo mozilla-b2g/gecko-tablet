261fd192b5d893fb831c405edb9c2590a10b3f01
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2016-05-02 10:46:24 +0200

    Bug 1266035 - Switch TV-related mochitests to chrome r=fabrice
    MozReview-Commit-ID: C2ezlZB9z0a

--- dom/tv/test/mochitest/head.js
+++ dom/tv/test/mochitest/head.js
@@ -1,13 +1,5 @@
 "use strict";
 
-function setupPrefsAndPermissions(callback) {
-  setupPrefs(function() {
-      SpecialPowers.pushPermissions([
-        {"type":"tv", "allow":1, "context":document}
-      ], callback);
-  });
-}
-
 function setupPrefs(callback) {
   let xhr = new XMLHttpRequest;
   let data;
@@ -27,14 +19,12 @@ function setupPrefs(callback) {
   });
 }
 
-function removePrefsAndPermissions(callback) {
-  SpecialPowers.popPrefEnv(function() {
-    SpecialPowers.popPermissions(callback);
-  });
+function removePrefs(callback) {
+  SpecialPowers.popPrefEnv(callback);
 }
 
 function prepareTest(callback) {
-  removePrefsAndPermissions(function() {
-    setupPrefsAndPermissions(callback);
+  removePrefs(function() {
+    setupPrefs(callback);
   });
 }
