017fb8bc7e9889542fb694d578bda1294d6f8188
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2016-06-16 23:06:28 +0200

    Basic support for sound volume changing
    MozReview-Commit-ID: 6ad7cdk1jF6

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1076,7 +1076,7 @@ pref("layers.compositor-lru-size", 10);
 pref("dom.vr.cardboard.enabled", true);
 
 // In B2G by deafult any AudioChannelAgent is muted when created.
-pref("dom.audiochannel.mutedByDefault", false);
+pref("dom.audiochannel.mutedByDefault", true);
 
 // The app origin of bluetooth app, which is responsible for listening pairing
 // requests.
--- b2g/chrome/content/system/js/SoundManager.js
+++ b2g/chrome/content/system/js/SoundManager.js
@@ -12,18 +12,20 @@ var SoundManager = {
     'system': 0
   },
 
+  delta: 0.025,
+
   getMaxVolume: function(level) {
-     return Math.min(15, level);
+    return Math.min(1, level);
   },
 
   getMinVolume: function(level) {
-     return Math.max(0, level);
+    return Math.max(0, level);
   },
 
   handleVolumeUp: function() {
     console.debug('XXX SET THE VOLUME <<<UP>>> §§§§ !!!');
     Object.keys(this._channelsVolume).forEach(channel => {
-      var level = this.getMaxVolume(this._channelsVolume[channel] + 1);
+      var level = this.getMaxVolume(this._channelsVolume[channel] + this.delta);
       this.setVolume(channel, level);
     });
   },
@@ -31,23 +33,19 @@ var SoundManager = {
   handleVolumeDown: function() {
     console.debug('XXX SET THE VOLUME <<<DOWN>>> §§§§ !!!');
     Object.keys(this._channelsVolume).forEach(channel => {
-      var level = this.getMinVolume(this._channelsVolume[channel] - 1);
+      var level = this.getMinVolume(this._channelsVolume[channel] - this.delta);
       this.setVolume(channel, level);
     });
   },
 
   setVolume: function(channel, level) {
-    var key = 'audio.volume.' + channel;
-    this.settings.createLock().set({key: level});
     console.debug('XXX SET THE VOLUME TO ', level);
     this._channelsVolume[channel] = level;
-  },
 
-  handleSettingsChange: function(obj) {
-    console.debug("Got settings change: ", JSON.stringify(obj));
-    var channel = obj.settingName.split('.')[2];
-    this._channelsVolume[channel] = obj.settingValue;
-    console.debug("Applied settings change: ", JSON.stringify(this._channelsVolume));
+    var e = new CustomEvent('_setvolume', {
+      detail: { level: parseFloat(level) }
+    });
+    window.dispatchEvent(e);
   },
 
   start: function() {
@@ -56,36 +54,25 @@ var SoundManager = {
     window.addEventListener('_volumedown', this.handleVolumeDown.bind(this));
 
     if (navigator.mozAudioChannelManager) {
-      navigator.mozAudioChannelManager.volumeControlChannel = 'normal';
+      // navigator.mozAudioChannelManager.volumeControlChannel = 'normal';
       navigator.mozAudioChannelManager.allowedAudioChannels.forEach(ch => {
         console.debug("ALLOWED AUDIO CHANNEL: name=", ch.name);
-        ch.setVolume(0.25);
-        ch.setMuted(false);
+        ch.setVolume(0.95);
+        // ch.setMuted(false);
         ch.onactivestatechanged = ch_evt => {
-          console.debug("AudioChannel activestatechanged: ", ch_evt.name);
+          console.debug("AudioChannel activestatechanged: ", ch_evt.name, ch.name);
         };
         ch.getVolume().then(v => {
-          console.debug("name=", name, "volume=", v);
+          console.debug("name=", ch.name, "volume=", v);
           ch.getMuted().then(m => {
-            console.debug("name=", name, "muted=", m);
+            console.debug("name=", ch.name, "muted=", m);
             ch.isActive().then(a => {
-              console.debug("name=", name, "active=", a);
+              console.debug("name=", ch.name, "active=", a);
             });
           });
         });
       });
     }
-
-    Object.keys(this._channelsVolume).forEach(channel => {
-      var key = 'audio.volume.' + channel;
-      this.settings.addObserver(key, this.handleSettingsChange.bind(this));
-      console.debug('Reading setting value:', key);
-      this.settings.createLock().get(key).then(result => {
-        console.debug('Read setting value: ', key, result[key]);
-        this._channelsVolume[channel] = parseFloat(result[key]) || 0;
-        console.debug('Updated setting value: ', JSON.stringify(this._channelsVolume));
-      });
-    });
 
     return this;
   }
